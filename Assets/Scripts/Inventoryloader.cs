f0d17aecf7e5ae1419bcc62c6bf7d575ff56d5ea
connorbews <connbews@telus.net>
2020-10-10 11:04:49 -0700

    added title and pause screen

---
+++ Assets/Scripts/Inventoryloader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventoryloader : MonoBehaviour
+{
+    public static bool GameIsInventory = false;
+    public GameObject InventoryMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (GameIsInventory)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    void Resume()
+    {
+        InventoryMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsInventory = false;
+    }
+    void Pause()
+    {
+        InventoryMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsInventory = true;
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    public Button StartGame;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        GameIsPaused = false;
+        Time.timeScale = 1f;
+        Debug.Log("You have clicked the button!");
+        //SceneManager.LoadScene("StartScreen");
+        LevelLoader ll = GameObject.FindGameObjectWithTag("LevelLoader").GetComponent<LevelLoader>();
+        ll.LoadNextLevel("StartScreen");
+    }
+}
