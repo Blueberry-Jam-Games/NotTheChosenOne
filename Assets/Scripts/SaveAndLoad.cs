186b13c3c314d580bdd4d8e7e932ea93603aac89
connorbews <connbews@telus.net>
2020-10-21 00:48:08 -0700

    save and load working

---
+++ Assets/Scripts/SaveAndLoad.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Hosting;
+using System.Diagnostics;
+
+public static class SaveAndLoad
+{
+    
+    public static void Save(GameObject player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = @"C:\Users\Admin\Desktop\NotTheChosenOne\NotTheChosenOne\Assets\Scripts\save.txt";
+        FileStream fileStream = new FileStream(path, FileMode.Create);
+        PlayerData player1 = new PlayerData(player);
+        formatter.Serialize(fileStream, player1);
+        fileStream.Close();
+    }
+    public static PlayerData Load()
+    {
+        string path = @"C:\Users\Admin\Desktop\NotTheChosenOne\NotTheChosenOne\Assets\Scripts\save.txt";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream fileStream = new FileStream(path, FileMode.Open);
+            PlayerData player1 = formatter.Deserialize(fileStream) as PlayerData;
+            fileStream.Close();
+            return player1;
+        }
+        else
+        {
+            UnityEngine.Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+}
